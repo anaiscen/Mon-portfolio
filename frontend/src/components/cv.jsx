9e1d571bb5feffcf22fc00739925ffb2a037ff7b
AnaisCen <anaiscenz@gmail.com>
2023-06-14 10:57:02 +0200

    commiting

---
+++ frontend/src/components/cv.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const cv = () => {
+  return <div />;
+};
+
+export default cv;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Counter from "../components/Counter";
+import Counter from "../components/cv";
 import logo from "../assets/logo.svg";
 
 export default function Home() {
